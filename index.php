e0aaaa49a9b3481497b481bebc74ce4495b03883
marcbab01 <mobabin1993@gmail.com>
2025-02-09 21:13:12 -0500

    ajout sur Wordpress

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <div><?php the_content(); ?></div>
+    </article>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
