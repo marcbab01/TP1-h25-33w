d51ec4011454948ba22655b8b4a383d19a909afb
marcbab01 <mobabin1993@gmail.com>
2025-02-17 14:58:11 -0500

    organisation

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <article>
-        <h2><?php the_title(); ?></h2>
-        <div><?php the_content(); ?></div>
-    </article>
-<?php endwhile; endif; ?>
-<?php get_footer(); ?>
