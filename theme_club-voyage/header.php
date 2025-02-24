7f479d11ebe24abc03412a1e87430423e6e43ff8
marcbab01 <mobabin1993@gmail.com>
2025-02-23 21:49:30 -0500

    modif du menu de navigation

--- theme_club-voyage/header.php
+++ theme_club-voyage/header.php
@@ -20,10 +20,20 @@
     </header>
     <nav class="menu">
             <ul class="menu__contenu">
-              <li class="menu__li"><a href="#" class="menu__a">Promotions</a></li>
-              <li class="menu__li"><a href="#" class="menu__a">Reservation</a></li>
-              <li class="menu__li"><a href="#" class="menu__a">A Propos</a></li>
-              <li class="menu__li"><a href="#" class="menu__a">Evenements</a></li>
-              <li class="menu__li"><a href="#" class="menu__a">Contactez-nous</a></li>
+            <?php
+            wp_nav_menu(array(
+              'menu' => 'principal',
+              'theme_location' => 'main-menu',
+              'container' => false,
+            ));
+            ?>
             </ul>
+
+            <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+              <label>
+                <span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span>
+                <input type="search" class="search-field" placeholder="<?php echo esc_attr_x('Rechercher des destinations...', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" />
+              </label>
+              <button type="submit" class="search-submit"><?php echo esc_html_x('Search', 'submit button') ?></button>
+            </form>
     </nav>
