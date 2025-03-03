7835ab3453aae94f6f07b90220254858ea47385d
marcbab01 <mobabin1993@gmail.com>
2025-03-02 20:28:43 -0500

    update - functions

--- theme_club-voyage/functions.php
+++ theme_club-voyage/functions.php
@@ -7,17 +7,12 @@
  * @since Club de Voyage 1.0
  */
 
-function club_de_voyage_setup() {
+function register_setup() {
     add_theme_support('post-thumbnails');
 
     register_nav_menus(array(
-        'main-menu' => __('Menu Principal', 'club-de-voyage'),
-        'footer-menu' => __('Menu Pied de Page', 'club-de-voyage'),
+        'main-menu' => __('Menu Principal'),
+        'footer-menu' => __('Menu Pied de Page'),
     ));
 }
 add_action('after_setup_theme', 'club_de_voyage_setup');
-
-function add_font_awesome() {
-    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
-}
-add_action('wp_enqueue_scripts', 'add_font_awesome');
