9f4fb3de11c05f8cea67bffdff3d1fdc1cea9d1a
marcbab01 <mobabin1993@gmail.com>
2025-03-10 15:56:17 -0400

    Creation de la page 404.php

---
+++ theme_club-voyage/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+    <main class="error">
+        <h1>Erreur 404</h1>
+        <p>La page est introuvable</p>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/404-robot.jpg" alt="erreur 404 image">
+    </main>
+<?php get_footer(); ?>
