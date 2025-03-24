d54e452d6fbc980d30b189d41318c93c3958745b
marcbab01 <mobabin1993@gmail.com>
2025-03-24 19:49:49 -0400

    Mise a jour du fichier functions.php

--- theme_club-voyage/functions.php
+++ theme_club-voyage/functions.php
@@ -6,7 +6,6 @@
  * @subpackage Club_de_Voyage
  * @since Club de Voyage 1.0
  */
-
 function register_setup() {
     add_theme_support('post-thumbnails');
 
@@ -16,3 +15,16 @@ function register_setup() {
     ));
 }
 add_action('after_setup_theme', 'club_de_voyage_setup');
+
+$functions_dir = get_template_directory() . '/functions/';
+include_once $functions_dir . 'customizer.php';
+
+$function_files = array(
+    'customizer.php',
+    'scripts.php',
+    'widgets.php',
+);
+
+foreach ($function_files as $file) {
+    include_once $functions_dir . $file;
+}
