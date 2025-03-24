182a175c3b139e80a96ad23844855a870fabeff7
marcbab01 <mobabin1993@gmail.com>
2025-03-24 19:46:29 -0400

    modification de front-page.php

--- theme_club-voyage/front-page.php
+++ theme_club-voyage/front-page.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
 
 <main>
-      <section class="hero">
+<?php
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+$hero_telephone = get_theme_mod('hero_telephone', 'Default Title');
+$hero_background = get_theme_mod('hero_background', 'Default Title');
+?>
+<style>
+    .hero__couleur {
+        color: <?php echo get_theme_mod('hero_couleur', '#000000'); ?>;
+    }
+</style>
+      <section class="hero" style="background-image: url('<?php echo $hero_background; ?>')">
         <div class="hero__conteneur">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-landscape.jpg" alt=""class="hero__image">
           <div class="hero__texte">
@@ -10,14 +20,19 @@
             <br>
             <p>info@mondovoyages.com</p>
             <br>
-            <p>1 800-111-111</p>
+            <p class="hero__auteur">Auteur: <?php echo $hero_auteur; ?></p>
+            <br>
+            <p class="hero__auteur">Telephone: <?php echo $hero_telephone; ?></p>
             <br>
             <p>111 Rue Street, Montréal H1Z 2Z4</p>
+            <section class="hero__sociaux">
+              <?php get_template_part("gabarits/icone-sociaux"); ?>
+            </section>
           </div>
         </div>
       </section>
       <section class="gallerie">
-        <h1 class="gallerie__titre">En Vedette</h1>
+        <h1 class="gallerie__titre">Gallerie</h1>
         <div class="gallerie__contenu">
             <div><img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_promo1.jpg" alt="" class="medaillon_img"></div>
             <div><img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_promo1.jpg" alt="" class="medaillon_img"></div>
@@ -30,6 +45,7 @@
       </section>
 
       <section class="populaire">
+      <h1 class="gallerie__titre">Nos Destinations Populaires</h1>
         <div class="grille">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article class="carte">
