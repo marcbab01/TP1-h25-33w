e48ee173b3ec494d1f8821ce4cb10102eb559560
marcbab01 <mobabin1993@gmail.com>
2025-04-07 09:15:04 -0400

    error 404 - Update des fonctions dans customizer.php

--- theme_club-voyage/functions/customizer.php
+++ theme_club-voyage/functions/customizer.php
@@ -47,5 +47,53 @@ function theme_31w_customize_register($wp_customize) {
         'label' => __('Couleur du texte', 'theme_tp'),
         'section' => 'hero_section',
     )));
+
+    $wp_customize->add_section('error-404__section', array(
+        'title' => __('Section Error-404', 'theme_31w'),
+        'priority' => 40,
+    ));
+
+    $wp_customize->add_setting('error-404__titre', array(
+        'default' => __("Oops, vous avez échoué sur l'île 404 !", 'theme_31w'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    
+    $wp_customize->add_control('error-404__titre', array(
+        'label' => __('Error 404 Title', 'theme_31w'),
+        'section' => 'error-404__section',
+        'type' => 'text',
+    ));
+
+    $wp_customize->add_setting('error-404__message', array(
+        'default' => __("Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !", 'theme_31w'),
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+    
+    $wp_customize->add_control('error-404__message', array(
+        'label' => __('Error 404 Message', 'theme_31w'),
+        'section' => 'error-404__section',
+        'type' => 'text',
+    ));
+
+    $wp_customize->add_setting('error-404__background', array(
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw',
+    ));
+    
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'error-404__background', array(
+        'label' => __('Error-404 Background Image', 'theme_31w'),
+        'section' => 'error-404__section',
+    )));
+
+    $wp_customize->add_setting('error-404__homeLink', array(
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw',
+    ));
+    
+    $wp_customize->add_control('error-404__homeLink', array(
+        'label' => __('Error 404 Button', 'theme_31w'),
+        'section' => 'error-404__section',
+        'type' => 'url',
+    ));
 }
 add_action('customize_register', 'theme_31w_customize_register');
