8dcd5457d259489d61e9b879f04a6ecfd72c1125
marcbab01 <mobabin1993@gmail.com>
2025-04-07 08:29:41 -0400

    404 - Update du texte de la page 404.php

--- theme_club-voyage/404.php
+++ theme_club-voyage/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <main class="error">
-        <h1>Erreur 404</h1>
-        <p>La page est introuvable</p>
+        <h1>Oops, vous avez échoué sur l'île 404 !</h1>
+        <p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p>
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/404-robot.jpg" alt="erreur 404 image">
     </main>
 <?php get_footer(); ?>
